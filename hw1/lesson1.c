0bd8a7f477396106ba2fc96e005158592a514c12
July Sudarenko <july@sudarenko.ru>
2024-04-05 21:26:11 +0300

    first commit

---
+++ hw1/lesson1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+Комментарий
+*/
+int main(void)
+{
+/*
+(\___/)
+(='.'=)
+(")_(")
+*/
+    printf("(\\___/)\n");
+    printf("(='.'=)\n");
+    printf("(\")_(\")\n");
+    printf("Hello World!\n");
+    return 0;
+}
+
